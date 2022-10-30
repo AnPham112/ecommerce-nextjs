f01d816ddc7eb63088db52248af53960130431c5
AnPham <anpham1122000@gmail.com>
2022-10-30 16:53:11 +0700

    add button default

---
+++ components/elements/Button.jsx
@@ -0,0 +1,24 @@
+import clsx from "clsx";
+
+export const Button = ({
+    children,
+    onClick,
+    variant = "primary",
+    className
+}) => {
+    const defaultStyle = "h-10 px-5 rounded-lg";
+
+    const btnStyle = {
+        primary: "text-white bg-slate-800 hover:bg-slate-900",
+        secondary: "text-black bg-white hover:bg-slate-50",
+        dark: "text-white bg-black hover:bg-black-800 rounded-none"
+    }
+
+    return (
+        <button
+            onClick={onClick}
+            className={clsx(defaultStyle, btnStyle[variant], className)}>
+            {children}
+        </button>
+    )
+}
